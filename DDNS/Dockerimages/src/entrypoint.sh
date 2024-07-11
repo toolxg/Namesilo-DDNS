af9190cc363a9ab705637b76342e402f83ea0e24
Toolxg <mail@toolxg.com>
2024-07-11 17:01:48 +0800

    更改日志格式 环境变量错误

--- DDNS/Dockerimages/src/entrypoint.sh
+++ DDNS/Dockerimages/src/entrypoint.sh
@@ -12,9 +12,9 @@ trap handle_signal SIGTERM
 
 while [ 1 -eq 1 ]
 do
-    if [ -n "$Proxy" ]; then  
+    if [ -n "$PROXY" ]; then  
         cp /etc/proxychains/proxychains.conf /etc/proxysock5.conf
-        sed -i "s/^socks4 	127.0.0.1 9050/socks5 	${Proxy}/" "/etc/proxysock5.conf"  
+        sed -i "s/^socks4 	127.0.0.1 9050/socks5 	${PROXY}/" "/etc/proxysock5.conf"  
     fi  
     `/bin/bash namesiloddns-dk.sh`
     sleep ${LOOPTIME:-10}m
